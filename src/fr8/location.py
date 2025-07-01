cf5b52b56b80ffd0a319aad86aa67fc74329cd29
Kian Kyars <kiankyars@gmail.com>
2025-07-01 17:58:01 +0300

    5

--- src/fr8/location.py
+++ src/fr8/location.py
@@ -1,5 +1,6 @@
 from geopy.geocoders import Nominatim
 from geopy.distance import geodesic
+from geopy.exc import GeocoderTimedOut
 
 def calculate_distance(lat1: float, lon1: float, lat2: float, lon2: float) -> float:
     return geodesic((lat1, lon1), (lat2, lon2)).km
@@ -12,7 +13,7 @@ def get_location(address: str) -> tuple[float, float]:
 def fr8_location(address: str) -> float:
     try:
         location = get_location(address)
-    except TimeoutError:
+    except GeocoderTimedOut:
         return "address not found"
     fr8 = get_location("Otaranta 4, 02150 Espoo")
     return calculate_distance(location.latitude, location.longitude, fr8.latitude, fr8.longitude)
