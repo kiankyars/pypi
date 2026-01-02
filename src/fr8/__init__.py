f026bafc50da28fde3f502d90b3c5b14abdebf25
Kian Kyars <kiankyars@gmail.com>
2026-01-02 07:42:06 -0700

    Add CLI tool and add linter and lint readme as well as npxprittier

--- src/fr8/__init__.py
+++ src/fr8/__init__.py
@@ -1,1 +1,8 @@
 from fr8.location import fr8_location
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description="Calculate your distance from fr8.")
+    parser.add_argument("address", help="Address to calculate distance from")
+    args = parser.parse_args()
+    return fr8_location(args.address)
